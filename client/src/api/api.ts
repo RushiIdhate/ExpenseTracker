03655c7dcc4caad890718dce09828188f328a98e
RushiIdhate <144716042+RushiIdhate@users.noreply.github.com>
2026-09-24 22:38:33 +0530

    Updated Backend api url

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://expensetrackerserver-x3fs.onrender.com/api';
 
 export interface ApiError {
   message: string;
